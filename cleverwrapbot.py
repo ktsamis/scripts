cc625ba1c78a5e116bcc66c390909aaea71bfb2f
Konstantinos Tsamis <ktsamis@suse.com>
2017-07-19 18:00:27 +0200

    Script to talk to clever bot via its API

---
+++ cleverwrapbot.py
@@ -0,0 +1,13 @@
+from cleverwrap import CleverWrap
+
+"""This script allows you to talk to cleverbot 
+using an already existing APIKEY in the command line"""
+
+cw = CleverWrap("APIKEY")
+question = "nada"
+while not question == "finished":
+    question = input("You say: ")
+    answer = cw.say(question)
+    print("The bot says: " + answer)
+
+cw.reset()  # resets the conversation ID and conversation state.
